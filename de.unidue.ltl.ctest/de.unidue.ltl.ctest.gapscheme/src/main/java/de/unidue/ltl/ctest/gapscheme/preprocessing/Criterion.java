af7af7775eaf78a32330791bed77d75e96dc41a3
Marius Hamacher <marius.hamacher@outlook.de>
2018-08-15 15:42:38 +0200

    Add gapscheme classes.

---
+++ de.unidue.ltl.ctest/de.unidue.ltl.ctest.gapscheme/src/main/java/de/unidue/ltl/ctest/gapscheme/preprocessing/Criterion.java
@@ -0,0 +1,17 @@
+package de.unidue.ltl.ctest.gapscheme.preprocessing;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+
+//TODO: Remove. May be used later, but has no purpose at the moment.
+public interface Criterion extends Predicate<Token>{
+	public Collection<Class<? extends Annotation>> getRequiredAnnotations();
+	public AnalysisEngineDescription getEngineDescription(Class<? extends Annotation> requiredClass);
+}
---
+++ de.unidue.ltl.ctest/de.unidue.ltl.ctest.gapscheme/src/test/java/de/unidue/ltl/ctest/gapscheme/preprocessing/IsPunctuationTest.java
@@ -0,0 +1,29 @@
+package de.unidue.ltl.ctest.gapscheme.preprocessing;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.JCasBuilder;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import junit.framework.TestCase;
+
+public class IsPunctuationTest extends TestCase {
+
+	@Test
+	public void testPunctuation() throws UIMAException {
+		JCas jcas = JCasFactory.createJCas();
+		JCasBuilder jcasBuilder = new JCasBuilder(jcas);
+
+		Token punctuation = jcasBuilder.add("¿", Token.class);
+		Token word = jcasBuilder.add("a", Token.class);
+
+		jcasBuilder.close();
+
+		IsPunctuation criterion = new IsPunctuation();
+
+		assertFalse(criterion.test(word));
+		assertTrue(criterion.test(punctuation));
+	}
+}
